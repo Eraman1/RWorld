92111fda20bc3f7cc33c78fa34f6e6d1766606c9
Eraman1 <er.aman614@gmail.com>
2025-08-04 15:32:32 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // ✅ Add this line
+  base: "/", 
   plugins: [react()],
   resolve: {
     alias: {
